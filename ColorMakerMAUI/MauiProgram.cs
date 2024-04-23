a61cbdd39f2a8e96ed487d2324a48a6ca993b127
abdelillahbel <abdelillahbel@engineer.com>
2024-04-23 23:01:08 +0100

    App icon changed #1

--- ColorMakerMAUI/MauiProgram.cs
+++ ColorMakerMAUI/MauiProgram.cs
@@ -18,7 +18,7 @@ namespace ColorMakerMAUI
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
